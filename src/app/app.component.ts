2b9997609cbc46269abf1e84968e067285bcddb1
Paulo Diego <pdiegodo@gmail.com>
2024-04-21 18:26:08 -0300

    Cor rodape

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,12 @@
-// src/app/app.component.ts
-
 import { Component } from '@angular/core';
 import { FooterComponent } from './core/components/footer/footer.component';
-
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [FooterComponent] // Inclua o FooterComponent aqui
+  imports: [FooterComponent]
 })
 export class AppComponent {
   title = 'lisa';
