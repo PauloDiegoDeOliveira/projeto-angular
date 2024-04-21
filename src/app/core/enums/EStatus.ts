46c11ce9a458344f8d87d8a13503135e115eba4c
Paulo Diego <pdiegodo@gmail.com>
2024-04-21 15:25:42 -0300

    Clean code

--- src/app/core/enums/EStatus.ts
+++
@@ -1,8 +0,0 @@
-export enum EStatus {
-  Ativo = "Ativo",
-  Inativo = "Inativo"
-}
-
-export type Status = 'Ativo' | 'Inativo';
-
-export const status = Object.keys(EStatus).map((e: string) => { return { value: e, viewValue: EStatus[e as keyof typeof EStatus] } });
