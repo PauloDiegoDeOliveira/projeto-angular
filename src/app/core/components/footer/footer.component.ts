23c6cbb477d3acec496cb3ef52e0139b1e1e145a
Paulo Diego <pdiegodo@gmail.com>
2024-04-21 13:33:05 -0300

    Estrutura inicial

---
+++ src/app/core/components/footer/footer.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, RoutesRecognized } from '@angular/router';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
+})
+export class FooterComponent implements OnInit {
+
+  public display!: boolean;
+  public currentYear!: number;
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    this.verifyRoute();
+    this.getYear();
+  }
+
+  verifyRoute() {
+    this.router.events.subscribe(event => {
+      if (event instanceof RoutesRecognized) {
+        let route = event.state.root.firstChild;
+        this.display = route?.data['footer'] ?? true;
+      }
+    })
+  }
+
+  getYear(): void {
+    this.currentYear = new Date().getFullYear();
+  }
+
+  openPrivacyPolicy() {
+    window.open('https://www.teste.com.br/');
+  }
+
+}
